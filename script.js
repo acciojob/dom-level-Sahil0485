7a0f53d2deffd2af7c8f2460d0e1e9b90668e723
SAHIL THAKUR <sahil20120067926thakur@gmail.com>
2025-10-25 07:39:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Get the element with id="level"
+let element = document.getElementById("level");
+
+// Initialize level counter
+let level = 0;
+
+// Traverse up the DOM tree until there are no more parent nodes
+while (element) {
+    level++;
+    element = element.parentElement;
+}
+
+// Display the result
+alert("The level of the element is: " + level);
